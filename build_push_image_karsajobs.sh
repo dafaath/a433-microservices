680657745639f6911a284841e8922e941a5fa841
dafaath <dafa.athaullah@bytedance.com>
2024-06-23 00:22:07 +0700

    finishing task

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Build docker image
+docker build -t ghcr.io/dafaath/karsajobs:latest .
+
+# Login to github registry
+echo "$PASS_GITHUB" | docker login ghcr.io --username dafaath --password-stdin
+
+# Push to do github registry
+docker push ghcr.io/dafaath/karsajobs:latest 
+
+
